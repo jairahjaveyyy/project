0b9dec2a6857805b5a7c340579b9ecff1902b6bd
jairahjaveyyy <jairahjave@gmail.com>
2024-03-21 18:06:46 +0800

    first

--- activities/act1/app.js
+++ activities/act1/app.js
@@ -5,9 +5,9 @@ import { rightDiv } from "./img.js"
 
 const data = {
     title: "Tulip Flower",
-    para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus quisquam in alias odio placeat adipisci, corporis ex necessitatibus eum, sequi praesentium perspiciatis est error quaerat, voluptas unde consequuntur! Qui!",
+    para: "Tulips are spring-blooming perennial herbaceous bulbiferous geophytes in the Tulipa genus. Their flowers are usually large, showy, and brightly coloured, generally red, orange, pink, yellow, or white. They often have a different coloured blotch at the base of the tepals, internally.",
     title2: "About Tulip",
-    para2: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet delectus quisquam in alias odio placeat adipisci, corporis ex necessitatibus eum, sequi praesentium perspiciatis est error quaerat, voluptas unde consequuntur! Qui!",
+    para2: "The name tulip is thought to be derived from a Persian word for turban, which it may have been thought to resemble by those who discovered it.",
     button1: "Contact Us",
     button2: "About Us",
     button3: "Home",
--- activities/act2/js/app.js
+++ activities/act2/js/app.js
@@ -5,20 +5,20 @@ import {card} from "./card.js"
 
 const myObject = {
     title: "Hello World",
-    par: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus veritatis temporibus voluptatum modi quam delectus, unde sunt voluptas doloremque expedita cupiditate in error recusandae esse cumque odit maiores laudantium ea?",
+    par: "Hi, I am Jairah Jave Saluta Gallegos, a 20 years old from Medina Misamis Oriental. I am a second year student currently studying in Gingoog City Colleges taking up BS Information Techhnology.",
     button: "Contact Me",
-    img: "../images/imgformal.png"
+    img: "../images/imgformal.jpg"
 }
 
 const myObject2 = {
     title1: "Project Completed",
-    img1: "../images/project.jpg",
+    img1: "../images/project1.jpg",
     par1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus veritatis temporibus voluptatum modi quam delectus, unde sunt voluptas doloremque expedita cupiditate in error recusandae esse cumque odit maiores laudantium ea?",
     button1: "Learn More",
-    img2: "../images/project.jpg",
+    img2: "../images/project2.jpg",
     par2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus veritatis temporibus voluptatum modi quam delectus, unde sunt voluptas doloremque expedita cupiditate in error recusandae esse cumque odit maiores laudantium ea?",
     button2: "Learn More",
-    img3: "../images/project.jpg",
+    img3: "../images/project3.png",
     par3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus veritatis temporibus voluptatum modi quam delectus, unde sunt voluptas doloremque expedita cupiditate in error recusandae esse cumque odit maiores laudantium ea?",
     button3: "Learn More"
 
